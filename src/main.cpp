35c829fa58df7c11bb75c4e27461f963f6926788
Angel Ruiz Fernandez <arf20@desktop.arf20.com>
2022-12-27 22:46:17 +0100

    Add license notice to source files

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+/*
+ww1game:  Generic WW1 game (?)
+main.cpp: Entry point
+
+Copyright (C) 2022 Ángel Ruiz Fernandez
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifdef _WIN32
 	#define SDL_MAIN_HANDLED    // ah yes, windows shenanigans
 #endif
